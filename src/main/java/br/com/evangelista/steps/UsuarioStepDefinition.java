08a6708270e1e923712c55d7f4d82e455d43f70c
MatEvangelista <mathews.pee@gmail.com>
2021-02-15 15:37:15 -0300

    "Mapeamento da API"

--- src/main/java/br/com/evangelista/steps/UsuarioStepDefinition.java
+++ src/main/java/br/com/evangelista/steps/UsuarioStepDefinition.java
@@ -1,81 +1,34 @@
 package br.com.evangelista.steps;
 
- import br.com.evangelista.dominio.entidades.User;
- import gherkin.ast.DocString;
+import br.com.evangelista.dominio.api.UserApi;
+import br.com.evangelista.dominio.entidades.User;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
-import org.apache.http.HttpStatus;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class UsuarioStepDefinition {
 
-    private static final String CREATE_USER_ENDPOINT = "/v3/user";
-    private static final String USER_ENDPOINT = "/v3/user/{name}";
+    private User expectedUser;
+    private final UserApi userApi;
 
-    private Map<String, String> expectedUserTabela = new HashMap<>();
-    private User userTabela;
-
-    @Quando("faço um POST para {word} com os seguintes valores:")
-    public void facoUmPOSTSeguintesValores(String endpoint, Map<String, String> userTabela) {
-        expectedUserTabela = userTabela;
-
-        given()
-            .body(userTabela)
-        .when()
-            .post(endpoint)
-        .then()
-            .statusCode(HttpStatus.SC_OK);
-
-    }
-
-    @Então("quando faço um GET para {word}, o usuário criado é retornado")
-    public void quandoFacoUmGETuusuarioCriadoERetornado(String endpoint) {
-        when()
-            .get(endpoint)
-        .then()
-            .statusCode(HttpStatus.SC_OK)
-            .body("username", is(expectedUserTabela.get("username")));
-        // o primeiro username é do body da req e o segundo corresponde ao da tabela
-    }
-
-    @Quando("faço um POST para {word} com a seguinte docString:")
-    public void facoUmPOSTParaSeguinteDocString(String endpoint, DocString docString) {
-        expectedUserTabela.put("username", "zawahiri");
-
-        given()
-            .body(docString.getContent())
-        .when()
-            .post(endpoint)
-        .then()
-            .statusCode(HttpStatus.SC_OK);
+    public UsuarioStepDefinition() {
+        userApi = new UserApi();
     }
 
     @Quando("crio um usuário")
     public void crioUmUsuario() {
-        userTabela = User.builder().build();
-
-        given()
-            .body(userTabela)
-        .when()
-            .post(CREATE_USER_ENDPOINT)
-        .then()
-            .statusCode(200);
+        expectedUser = User.builder().build();
+        userApi.createUser(expectedUser);
     }
 
     @Então("o usuário é salvo no sistema")
     public void oUsuarioESalvoNoSistema() {
-        given()
-            .pathParam("name", userTabela.getUsername())
-        .when()
-            .get(USER_ENDPOINT)
-        .then()
-            .statusCode(HttpStatus.SC_OK)
-            .body("username", is(userTabela.getUsername()));
+        String actualUsername = userApi.getNicknameUsuario(expectedUser);
+        assertThat(actualUsername, is(expectedUser.getUsername()));
+
     }
+
+
 }
